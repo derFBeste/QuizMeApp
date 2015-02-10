aa0d3dc57c89444c9da2cc38bf382f167a039768
derFBeste <freddybeste@gmail.com>
2015-02-10 13:26:11 -0500

    Intial Commit

---
+++ src/com/quizMe/app/QuizMeFredBesteman.java
@@ -0,0 +1,15 @@
+package com.quizMe.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class QuizMeFredBesteman extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_quiz_me_fred_besteman);
+	}
+}
